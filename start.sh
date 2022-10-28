f8d7994e67f16aa751dd2dc6eb2d3d809a354984
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2022-10-28 10:52:57 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hansaka-Anuhas-1/Manual-Filter-Bot.git /Manual-Filter-Bot
+  git clone https://github.com/sittpaing123/Manual-Filter-Bot.git /Manual-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Manual-Filter-Bot
